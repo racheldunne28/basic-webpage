0c3a2338318b0d47b4a323e2c22816457d53291e
racheldunne-CO <rachel.dunne@cabinetoffice.gov.uk>
2021-02-21 14:27:10 +0000

    Feat: Function for getting new list value from text input

--- app.js
+++ app.js
@@ -13,3 +13,9 @@ switcher.addEventListener('click', function() {
     }
     console.log('current class name: ' + className);
 });
+
+function getNewListValue(){
+    // Selecting the input element and get its value 
+    var inputVal = document.getElementById("myInput").value;
+    console.log("New list value is " + inputVal);
+}
